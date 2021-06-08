8bb587ef180409de17e157aecd8e74a5f767d2fa
Gustavo Toledo <toledo31@usp.br>
2021-06-08 10:19:14 -0300

    Generating my first code extracting some information about Palmeiras' titles

---
+++ PalmeirasTitles/PalmeirasTitles/spiders/Palmeiras.py
@@ -0,0 +1,37 @@
+import scrapy
+
+
+class PalmeirasSpider(scrapy.Spider):
+    name = 'Palmeiras'
+    start_urls = [
+        'https://pt.wikipedia.org/wiki/Sociedade_Esportiva_Palmeiras']
+
+    def parse(self, response):
+        for i in response.xpath('//*[@id="mw-content-text"]/div[1]/table[6]/tbody/tr[@bgcolor="#f9f9f9"]'):
+            var = 1
+            if len(i.xpath('./td')) == 4:
+                var += 1
+                comp = i.xpath(f'./td[{var}]/b/a/text()').get()
+                print(comp)
+                var += 1
+                tit = i.xpath(f'./td[{var}]/b/text()').get()
+                print(tit)
+                var += 1
+                temp = i.xpath(f'./td[{var}]/b/a/text()').get()
+                print(temp)
+
+            elif len(i.xpath('./td')) == 3:
+                comp = i.xpath(f'./td[{var}]/b/a/text()').get()
+                print(comp)
+                var += 1
+                tit = i.xpath(f'./td[{var}]/b/text()').get()
+                print(tit)
+                var += 1
+                temp = i.xpath(f'./td[{var}]/b/a/text()').get()
+                print(temp)
+
+            yield {
+                'Competição': comp,
+                'Títulos': tit,
+                'Temporadas': temp
+            }
